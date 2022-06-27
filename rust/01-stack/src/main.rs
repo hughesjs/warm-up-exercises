f2d86c17372de514aa253dcd9b31075a7686c15c
James Hughes <james.hughes@codurance.com>
2022-06-28 00:07:36 +0100

    Added rest of stack methods and refactored tests
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- rust/01-stack/src/main.rs
+++ rust/01-stack/src/main.rs
@@ -1,15 +1,36 @@
 fn main() {
-    let mut stack = Stack::new();
+    can_count_items(&get_test_stack());
+    can_peek_without_removing(&get_test_stack());
+    can_pop_corectly(&get_test_stack());
+    returns_none_when_empty(&get_test_stack());
+}
+
+fn get_test_stack() -> Stack<i32> {
+    let mut stack: Stack<i32> = Stack::new();
+    
     stack.push(42);
     stack.push(555);
     stack.push(666);
 
-    // Check I can push and pop everything correctly
+    return stack;
+}
+
+fn can_count_items(stack: &Stack<i32>){
+    assert!(stack.length() == 3);
+}
+
+fn can_peek_without_removing(stack: &Stack<i32>){
+    assert!(stack.peek().unwrap() == 666);
+    assert!(stack.length() == 3);
+}
+
+fn can_pop_corectly(stack: &Stack<i32>){
     assert!(stack.pop().unwrap() == 666);
     assert!(stack.pop().unwrap() == 555);
     assert!(stack.pop().unwrap() == 42);
+}
 
-    // Check it returns none when the stack is empty (no exceptions in rust)
+fn returns_none_when_empty(stack: &Stack<i32>){
     assert!(stack.pop().is_none());
 }
 
@@ -30,5 +51,20 @@ impl<T> Stack<T> {
     fn push(&mut self, item: T) {
         self.stack.push(item);
     }
-}
 
+    fn clear(&mut self){
+        self.stack.clear();
+    }
+
+    fn length(&self) -> usize{
+        return self.stack.len();
+    }
+
+    fn peek(&self) -> Option<&T> {
+        return self.stack.last();
+    }
+
+    fn is_empty(&self) -> bool {
+       return self.stack.is_empty();
+    }
+}
