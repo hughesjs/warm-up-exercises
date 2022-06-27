f2f7d73cda897d81d0b47124edc3b8995d1ba988
James Hughes <james.hughes@codurance.com>
2022-06-27 17:39:22 +0100

    Added push and pop with tests
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- rust/01-stack/README.md
+++ rust/01-stack/README.md
@@ -4,4 +4,4 @@ Implement a Stack class with the following public methods:
 - void push(Object object)
 - Object pop()
 
-Stack should throw an exception if popped when empty.
+Stack should throw an exception if popped when empty. 
--- rust/01-stack/src/main.rs
+++ rust/01-stack/src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut stack = Stack::new();
+    stack.push(42);
+    stack.push(555);
+    stack.push(666);
+
+    // Check I can push and pop everything correctly
+    assert!(stack.pop().unwrap() == 666);
+    assert!(stack.pop().unwrap() == 555);
+    assert!(stack.pop().unwrap() == 42);
+
+    // Check it returns none when the stack is empty (no exceptions in rust)
+    assert!(stack.pop().is_none());
 }
 
 
@@ -11,4 +22,13 @@ impl<T> Stack<T> {
     fn new() -> Self {
         Stack {stack: Vec::new()}
     }
+
+    fn pop(&mut self) -> Option<T> {
+        return self.stack.pop();
+    }
+
+    fn push(&mut self, item: T) {
+        self.stack.push(item);
+    }
 }
+
