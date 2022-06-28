cb15a1e20dc1b30140f6cb71d024c622284b5f84
James Hughes <james.hughes@codurance.com>
2022-06-28 13:44:55 +0100

    Can parse single digits
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- 01-tdd-practice-roman-numerals/rust/src/lib.rs
+++ 01-tdd-practice-roman-numerals/rust/src/lib.rs
@@ -1,8 +1,31 @@
+fn covert_from_roman(roman: &str) -> i32 {
+    return match roman{
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000,
+        &_ => 0
+    };
+    panic!();
+}
+
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    use super::*;
+    use test_case::test_case;   
+
+    #[test_case("I", 1 ; "when single digit I")]
+    #[test_case("V", 5 ; "when single digit V")]
+    #[test_case("X", 10 ; "when single digit X")]
+    #[test_case("L", 50 ; "when single digit L")]
+    #[test_case("C", 100 ; "when single digit C")]
+    #[test_case("D", 500 ; "when single digit D")]
+    #[test_case("M", 1000 ; "when single digit M")]
+    fn it_can_convert_from_roman_numerals(roman: &str, val: i32){
+        assert!(covert_from_roman(roman) == val);
     }
 }
