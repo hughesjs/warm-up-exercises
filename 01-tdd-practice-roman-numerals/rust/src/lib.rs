dc6aa6e84ecad7409021aa12be3f6fb297b85963
James Hughes <james.hughes@codurance.com>
2022-06-28 13:53:03 +0100

    Can parse simple compound numbers

--- 01-tdd-practice-roman-numerals/rust/src/lib.rs
+++ 01-tdd-practice-roman-numerals/rust/src/lib.rs
@@ -1,9 +1,14 @@
 fn covert_from_roman(roman: &str) -> i32 {
-    return convert_single_digit(roman.chars().next().unwrap())
+    let mut acc: i32 = 0;
+    for numeral in roman.chars() {
+        acc += convert_single_numeral(numeral);
+    }
+    return acc;
 }
 
-fn convert_single_digit(roman: char) -> i32 {
-    return match roman{
+
+fn convert_single_numeral(roman: char) -> i32 {
+    return match roman {
         'I' => 1,
         'V' => 5,
         'X' => 10,
@@ -11,24 +16,30 @@ fn convert_single_digit(roman: char) -> i32 {
         'C' => 100,
         'D' => 500,
         'M' => 1000,
-        _ => 0
+        _ => 0,
     };
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use test_case::test_case;   
+    use test_case::test_case;
 
-    #[test_case("I", 1 ; "when single digit I")]
-    #[test_case("V", 5 ; "when single digit V")]
-    #[test_case("X", 10 ; "when single digit X")]
-    #[test_case("L", 50 ; "when single digit L")]
-    #[test_case("C", 100 ; "when single digit C")]
-    #[test_case("D", 500 ; "when single digit D")]
+    #[test_case("I", 1    ; "when single digit I")]
+    #[test_case("V", 5    ; "when single digit V")]
+    #[test_case("X", 10   ; "when single digit X")]
+    #[test_case("L", 50   ; "when single digit L")]
+    #[test_case("C", 100  ; "when single digit C")]
+    #[test_case("D", 500  ; "when single digit D")]
     #[test_case("M", 1000 ; "when single digit M")]
-    fn it_can_convert_from_roman_numerals(roman: &str, val: i32){
+    #[test_case("II", 2   ; "when compound II")]
+    #[test_case("III", 3  ; "when compound III")]
+    #[test_case("XX", 20  ; "when compound XX")]
+    #[test_case("XXX", 30 ; "when compound XXX")]
+    #[test_case("XII", 12 ; "when compound XII")]
+    #[test_case("CX", 110 ; "when compound CX")]
+    #[test_case("MMXXII", 2022 ; "when compound MMXXII")]
+    fn it_can_convert_from_roman_numerals(roman: &str, val: i32) {
         assert!(covert_from_roman(roman) == val);
     }
 }
