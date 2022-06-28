2df38693e49d642608bf2ec3b4198b6cab1141b8
James Hughes <james.hughes@codurance.com>
2022-06-28 15:01:25 +0100

    Can parse all permutations
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- 01-tdd-practice-roman-numerals/rust/src/lib.rs
+++ 01-tdd-practice-roman-numerals/rust/src/lib.rs
@@ -1,9 +1,24 @@
-fn covert_from_roman(roman: &str) -> i32 {
+fn convert_from_roman(roman: &str) -> i32 { 
+    let mut iter = roman.chars().peekable();
     let mut acc: i32 = 0;
-    for numeral in roman.chars() {
-        acc += convert_single_numeral(numeral);
+    while let Some(c) = iter.next() {
+        acc += get_acc_value(c, *iter.peek().unwrap_or(&'∅'));
     }
     return acc;
+}
+
+fn get_acc_value(current: char, next: char) -> i32 {
+    let current_i: i32 = convert_single_numeral(current);
+    let next_i: i32 = convert_single_numeral(next);
+
+    if current_i >= next_i
+    {
+        return current_i;
+    }
+    else
+    {
+        return -current_i;
+    }
 }
 
 
@@ -24,6 +39,7 @@ fn convert_single_numeral(roman: char) -> i32 {
 mod tests {
     use super::*;
     use test_case::test_case;
+    use roman;
 
     #[test_case("I", 1    ; "when single digit I")]
     #[test_case("V", 5    ; "when single digit V")]
@@ -39,7 +55,23 @@ mod tests {
     #[test_case("XII", 12 ; "when compound XII")]
     #[test_case("CX", 110 ; "when compound CX")]
     #[test_case("MMXXII", 2022 ; "when compound MMXXII")]
+    #[test_case("IV", 4 ; "when compound IV")]
+    #[test_case("IX", 9 ; "when compound IX")]
+    #[test_case("XIV", 14 ; "when compound XIV")]
+    #[test_case("XIX", 19 ; "when compound XIX")]
+    #[test_case("CM", 900 ; "when compound CM")]
+    #[test_case("MMCD", 2400 ; "when compound MMCD")]
     fn it_can_convert_from_roman_numerals(roman: &str, val: i32) {
-        assert!(covert_from_roman(roman) == val);
+        assert!(convert_from_roman(roman) == val);
+    }
+
+    #[test]
+    fn it_can_convert_every_roman_numeral_to_3999() {
+        for i in 1..4000
+        {
+            let testRoman: &str = &roman::to(i).unwrap(); // Known-good crate
+            let res: i32 = convert_from_roman(testRoman);
+            assert!(res == i)
+        }
     }
 }
