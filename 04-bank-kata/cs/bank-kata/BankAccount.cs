49b22e340c4d825f0cf0f101704ce0ba9fcdbf91
James Hughes <james.hughes@codurance.com>
2022-06-28 23:55:32 +0100

    Refactored to keep single responsibility

--- 04-bank-kata/cs/bank-kata/BankAccount.cs
+++ 04-bank-kata/cs/bank-kata/BankAccount.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using System.Text;
 using bank_kata.Interfaces;
 using bank_kata.Models;
+using bank_kata.Services;
 
 namespace bank_kata;
 
@@ -24,24 +25,6 @@ public class BankAccount: IBankAccount
 	public void PrintStatement()
 	{
 		IReadOnlyList<Transaction> transactions = _transactionRepository.GetTransactions();
-
-		decimal acc = 0;
-		IEnumerable<(Transaction, decimal)> transactionsWithBalance = transactions.Select(t =>
-		{
-			acc += t.Amount;
-			return (t, acc);
-		}).ToArray();
-
-		StringBuilder builder = new();
-		builder.Append(CultureInfo.CurrentCulture, $"{"DATE",-11}| {"AMOUNT",-8}| {"BALANCE",-7}\n");
-
-		foreach ((Transaction Tx, decimal Balance) txBalancePair in transactionsWithBalance.Reverse())
-		{
-			builder.AppendFormat(CultureInfo.CurrentCulture, $"{txBalancePair.Tx.TxTime,-11:dd/MM/yyyy}| {txBalancePair.Tx.Amount,-8:F2}| {txBalancePair.Balance, -7:F2}\n");
-		}
-
-		byte[] bytes = Encoding.UTF8.GetBytes(builder.ToString());
-		
-		_outputStream.Write(bytes, 0, bytes.Length);
+		StatementPrinter.Print(_outputStream, transactions);
 	}
 }
